7b9101cd9f8c34719e6b3687bfa939eda57a7923
Vladimir Lukyanov <vladimir@lukyanov.net>
2025-08-19 20:18:17 +0000

    v0.1.2
    - mysql connection fix

--- .conf/wordpress/wp-config.php
+++ .conf/wordpress/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', $_ENV['DB_USER'] );
 define( 'DB_PASSWORD', $_ENV['DB_PASSWORD'] );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'database' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
--- test.php
+++ test.php
@@ -6,10 +6,6 @@ echo "<br>";
 
 echo "This is a test page for the FrankenWP Dev Implementation.";
 
-$conn = mysqli_connect("127.0.0.1", "user", "password", "wordpress");
-var_dump($conn);
-
-
 phpinfo();
 
 xdebug_info();
